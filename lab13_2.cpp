f26100e33e341d017932bc6e61f9134cdef12df1
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-01-05 02:00:58 +0700

    lab13_2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool u[][M],int s,int x,int y){
+    for(int row = 0; row < N;row++){
+            for(int colum = 0;colum < M;colum++){
+                if(sqrt(pow(row - x,2) + pow(colum - y,2)) <= s-1){
+                u[row][colum] = 1;
+                }
+            }
+    }
+}
+
+void showImage(const bool a[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int row = 0; row < N;row++){
+            cout << "|";
+            for(int colum = 0;colum < M;colum++){
+                int n = a[row][colum];
+                if(n == 1)
+                cout << "*";
+                else cout << " ";
+            }
+            cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
