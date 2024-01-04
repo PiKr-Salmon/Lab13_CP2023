631f4802c14ae75138fecd365c6c14b55384073d
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-01-05 01:59:07 +0700

    lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,32 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int i,double y[]){
+    double sum = 0,harmosum = 0 ;
+    double geosum = 1;
+    for(int cou = 0; cou < i; cou++ ){
+    sum = sum + (x[cou]);
+    harmosum = harmosum + (1 / x[cou]);
+    }
+    y[0] = sum/i ;
+
+    double Sd = 0;
+    for(int couu = 0; couu < i; couu++){
+        Sd = Sd + pow(x[couu] - y[0],2.0);
+    }
+    y[1] = sqrtl( Sd / i);
+
+    for(int cou = 0; cou < i; cou++ ){
+    geosum = geosum * (x[cou]);}
+    y[2] = pow(geosum,(1.0 / i));
+
+    y[3] = i/harmosum;
+
+     double max = x[0],min = x[0];
+    for(int findmm = 1 ; findmm < i ; findmm++ ){
+        if(x[findmm]>max) max = x[findmm];
+        if(x[findmm]<min) min = x[findmm];
+    }
+        y[4] = max ; y[5] = min ;
+}
